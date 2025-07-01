803632047fc5159780007a48c07be5d5c12e8253
AaronT1106 <Aatran0711@gmail.com>
2025-07-01 16:24:47 -0400

    Add files via upload

---
+++ 4-1. Pizzas.py
@@ -0,0 +1,4 @@
+pizzas = ['meat lover', 'Hawaiian', 'cheese']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza")
+print("I like pizza a lot, I normally eat pizza when I am having a watch party with friends!")
---
+++ 4-2. Animals.py
@@ -0,0 +1,4 @@
+animals=['Tiger','Lion','Panther']
+for animal in animals:
+    print(f"A {animal} would not make a great pet")
+print(f"All these animals are cats!")
